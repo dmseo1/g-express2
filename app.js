094065db498d6fe6e9b7089a283fe496cbc466ae
dmseo1 <dkfk2747@gmail.com>
2019-12-27 18:08:51 +0900

    router middleware enrolled

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var app = express();
 var path = require('path');
 require('dotenv').config();
 
+var indexRouter = require('./router/index'); // ./router/ 라고만 적어도 된다. index라는 명칭은 자동으로 인식하기 때문이다.
+var userRouter = require('./router/user');
+
+
 //[기초세팅]
 //뷰 템플릿 기본 폴더 지정
 app.set('views', path.join(__dirname, 'views'));    //views 폴더는 ~~ 여기예요~~~
@@ -16,6 +20,10 @@ app.set('html', require('ejs').renderFile);
 app.use(express.json())
 //정적파일 기본경로 등록을 위한 미들웨어 등록
 app.use(express.static(path.join(__dirname, 'public')));
+
+//라우터 등록
+app.use('/', indexRouter);
+app.use('/people', userRouter);
 
 
 var port = process.env.PORT || 3000;
