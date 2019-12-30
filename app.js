af19a8b796ca4275ebabc5e10e5936e70ba20a08
dmseo1 <dkfk2747@gmail.com>
2019-12-30 12:17:10 +0900

    12:00

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ var userRouter = require('./router/user');
 //[기초세팅]
 //뷰(템플릿) 기본 폴더 지정
 app.set('views', path.join(__dirname, 'views'));    //views 폴더는 ~~ 여기예요~~~
-//뷰 엔진을 ejs로 쓰겠다
+//__dirname은 현재 디렉토리의 절대경로를 저장하고 있는 변수
+
+//뷰 엔진 등록: 뷰 엔진을 ejs라는 것을 쓰겠다
 app.set('view engine', 'ejs');
 //html을 ejs를 이용하여 렌더링하겠다
 app.set('html', require('ejs').renderFile);
@@ -18,7 +20,7 @@ app.set('html', require('ejs').renderFile);
 //[미들웨어 등록]
 //express에서 json 처리를 해주도록 하기 위한 미들웨어 등록
 app.use(express.json())
-//정적파일 기본경로 등록을 위한 미들웨어 등록
+//정적파일 기본경로 등록을 위한 미들웨어 등록. serve-static 라이브러리를 이용하거나, 아래와 같이 한다
 app.use(express.static(path.join(__dirname, 'public')));
 //라우터 등록
 app.use('/', indexRouter);
@@ -27,9 +29,11 @@ app.use('/people', userRouter);
 
 var port = process.env.PORT || 3000;
 
-//원래 순서는 포트, url, 백로그, 콜백함수
+//원래 순서는 포트, url(기본값 localhost), 백로그(기본값 5000), 콜백함수
 //var url = '192.32.32.32'
 //app.listen(port, url, backlog, callback);
-app.listen(port, () => {
+//콜백 함수 full : err, req, res, next
+// 
+app.listen(port, (req, res) => {
     console.log(`서버가 시작되었습니다: http://localhost:${port}`);
 });
--- router/index.js
+++ router/index.js
@@ -3,11 +3,15 @@ var express = require('express');
 var router = express.Router();
 
 /* Get HomePage */
-router.get('/', (_, res) => {
+
+//라우팅 함수 get(<경로>, 콜백함수);
+//콜백함수 full parameter: (err, req, res, next)
+router.get('/', (req, res, next) => {
+  
     res.render('index', {
         title : "안녕하세요~"
     });
-});
 
+ });
 
 module.exports = router;
